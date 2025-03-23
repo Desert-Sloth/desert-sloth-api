586bf47634abb0ae03c0bc5e102a169977ea7ce5
Cade Erickson <erickson.cade04@gmail.com>
2025-03-23 18:23:27 -0400

    sqlite db connectivity. p.21

--- desert.sloth.Api/Controllers/CatalogController.cs
+++ desert.sloth.Api/Controllers/CatalogController.cs
@@ -1,20 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using desert.sloth.Domain.Catalog;
+using desert_sloth.Data;
 
 namespace desert.sloth.Api.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("/catalog")]
     public class CatalogController : ControllerBase
     {
+        private readonly StoreContext _db;
+
+        public CatalogController(StoreContext db){
+            _db = db;
+        }
+
         [HttpGet]
         public IActionResult GetItems(){
-            var items = new List<Item>(){
+            return Ok(_db.Items);
+
+           /* var items = new List<Item>(){
               new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m),
               new Item("Shorts", "Ohio State shorts.", "Nike", 44.99m)
             };
 
-            return Ok(items);
+            return Ok(items); */
         }
         [HttpGet("{id:int}")]
         public IActionResult GetItem(int id){
